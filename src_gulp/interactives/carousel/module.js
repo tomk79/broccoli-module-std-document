e9ceef000f97aa868bb98a66e387c64db19ae157
Tomoya Koyanagi <tomk79@gmail.com>
2019-02-14 20:35:34 +0900

    <carousel> tiny-slider を導入

---
+++ src_gulp/interactives/carousel/module.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', function(e){
+	var carouselModules = document.querySelectorAll('.px2-carousel');
+	for(var idx = 0; idx < carouselModules.length; idx ++){
+		// console.log(idx);
+	}
+});
