c7c5fa17129f76ba6eb1fc9be699b2e4d28806dc
Tomoya Koyanagi <tomk79@gmail.com>
2021-11-07 16:24:50 +0900

    画像モジュールと画像メディアモジュールに、「拡大表示」オプションを追加した。→動くようになった

--- modules/basics/image/module.js
+++ modules/basics/image/module.js
@@ -1,3 +1,14 @@
 window.Px2BroccoliModuleStdDocument_ImageScope = function(imgElm){
-    console.log('Px2BroccoliModuleStdDocument_ImageScope:', imgElm);
+    let imgSrc = imgElm.src;
+    let htmlSrc =
+`<div class="px2-image-scope">
+    <button type="button" class="px2-image-scope__close"></button>
+    <div class="px2-image-scope__inner">
+        <div class="px2-image-scope__img"><img src="${imgSrc}" alt="" /></div>
+    </div>
+</div>`;
+    imgElm.outerHTML += htmlSrc;
+    document.querySelector('.px2-image-scope__close').addEventListener('click', function(){
+        document.querySelector('.px2-image-scope').remove();
+    })
 }
