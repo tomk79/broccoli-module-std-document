a32af13de97559dead232302ac2f364b2287150f
Tomoya Koyanagi <tomk79@gmail.com>
2021-11-07 14:35:12 +0900

    画像モジュールと画像メディアモジュールに、「拡大表示」オプションを追加した。

--- modules/basics/image/module.js
+++ modules/basics/image/module.js
@@ -1,8 +1,3 @@
-(function(){
-/**
- * module: std-document:basics/image
- */
 window.Px2BroccoliModuleStdDocument_ImageScope = function(imgElm){
     console.log('Px2BroccoliModuleStdDocument_ImageScope:', imgElm);
 }
-})();
