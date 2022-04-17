9c74f05c0c8e288f4ca8e20a1f80089c11b35594
Tomoya Koyanagi <tomk79@gmail.com>
2022-04-17 13:40:30 +0900

    `.px2-viewport-fit`、 `.px2-image-scope`: 挙動の改善

--- modules/utils/viewport-fit/module.js
+++ modules/utils/viewport-fit/module.js
@@ -1,7 +1,7 @@
 let eventHandler = function(){
     document.querySelectorAll('.px2-viewport-fit__inner').forEach(( inner )=>{
-        inner.style.width = document.body.clientWidth + 'px';
         inner.style.left = (-1 * document.body.clientWidth / 2) + 'px';
+        inner.style.width = document.body.clientWidth + 'px';
     });
 };
 window.addEventListener('DOMContentLoaded', eventHandler);
