25969122a105755c35349ddd8a318c9e995e9926
Tomoya Koyanagi <tomk79@gmail.com>
2022-09-10 00:31:21 +0900

    px2style内 のすべてのモジュールを取り込んだ

--- src_px2style/px2style.js
+++ src_px2style/px2style.js
@@ -4,14 +4,17 @@
 module.exports = function(){
 }
 
-require('./_src/config/_config.js')(module.exports);
-require('./_src/modal/_modal.js')(module.exports);
-require('./_src/notice/_notice.js')(module.exports);
-require('./_src/styles/_loading.js')(module.exports);
-require('./_src/header/_header.js')(module.exports);
-
+// broccoli-module-std-document
+require('../modules/basics/image/module.js');
 require('../modules/interactives/accordion/module.js');
 require('../modules/interactives/carousel/module.js');
 require('../modules/interactives/tabs/module.js');
 require('../modules/utils/viewport-fit/module.js');
-require('../modules/basics/image/module.js');
+
+// px2style
+require('./_src/config/_config.js')(module.exports);
+require('./_src/header/_header.js')(module.exports);
+require('./_src/main.js')(module.exports);
+require('./_src/modal/_modal.js')(module.exports);
+require('./_src/notice/_notice.js')(module.exports);
+require('./_src/styles/_loading.js')(module.exports);
